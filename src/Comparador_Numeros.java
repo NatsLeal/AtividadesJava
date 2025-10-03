48146397be59bada00ae9bd3ab864d8e1cde1fa1
NatsLeal <nataliadsl_@hotmail.com>
2025-10-02 21:30:39 -0300

    Alteração

--- src/Comparador_Numeros.java
+++ src/Comparador_Numeros.java
@@ -1,11 +1,25 @@
 import java.util.Scanner;
 
-public class Calculadora_Soma {
+public class Comparador_Numeros {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
 
         System.out.println("Digite o primeiro número: ");
-        double numero1 = leitor.nextDouble();
+        int num1 = leitor.nextInt();
 
         System.out.println("Digite o segundo número: ");
-        double numero2 = leitor.nextDouble();
+        int num2 = leitor.nextInt();
+
+        if (num1 > num2) {
+            System.out.println("O número " + num1 +  " é maior.");
+        } else {
+            System.out.println("O número " + num2 +  " é maior.");
+        }
+
+        if (num1 == num2) {
+            System.out.println("Os números são iguais.");
+        }
+        leitor.close();
+
+    }
+}
